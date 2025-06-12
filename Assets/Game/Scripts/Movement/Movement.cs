85c74fbfb4fc35c16e6b1aefa9b7b16c99394115
WyzalDev <wyzaldev@gmail.com>
2025-06-13 00:22:03 +0300

    Add player movement

---
+++ Assets/Game/Scripts/Movement/Movement.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections;
+using Game.Scripts.Utility;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+namespace Game.Scripts.Movement
+{
+    [RequireComponent(typeof(Rigidbody2D))]
+    public class Movement : MonoBehaviour, IBlockable
+    {
+        [Header("Movement settings")]
+        [SerializeField] private float speed;
+        [Header("Dash settings")]
+        [SerializeField] private float dashForce;
+        [SerializeField] private float dashDuration;
+        [SerializeField] private float dashCooldown;
+        public bool IsBlocked { get; private set; }
+
+        private Rigidbody2D _rigidbody;
+        private InputAction _move;
+        private InputAction _dash;
+        private Vector2 _direction;
+        private Vector2 _lastDirection;
+        private Coroutine _dashCoroutine;
+        private void Awake()
+        {
+            _rigidbody = GetComponent<Rigidbody2D>();
+            IsBlocked = false;
+            _move = InputSystem.actions.FindAction("Move");
+            _dash = InputSystem.actions.FindAction("Dash");
+            _dash.performed += Dash;
+        }
+
+        public void FixedUpdate()
+        {
+            if(IsBlocked) return;
+
+            _rigidbody.linearVelocity = _direction * speed;
+            var angle = Mathf.Atan2(_direction.y, _direction.x) * Mathf.Rad2Deg;
+            _rigidbody.rotation = angle;
+        }
+
+        private void Update()
+        {
+            _direction = _move.ReadValue<Vector2>();
+            if(Vector2.zero != _direction) _lastDirection = _direction;
+        }
+
+        private void Dash(InputAction.CallbackContext context)
+        {
+            Debug.Log("Dash");
+            if(_dashCoroutine == null && !IsBlocked)
+                _dashCoroutine = StartCoroutine(DoDash());
+        }
+
+        private IEnumerator DoDash()
+        {
+            IsBlocked = true;
+            Debug.Log(_lastDirection.normalized);
+            _rigidbody.linearVelocity = _lastDirection.normalized * dashForce;
+            yield return new WaitForSeconds(dashDuration);
+            IsBlocked = false;
+            yield return new WaitForSeconds(dashCooldown);
+            _dashCoroutine = null;
+        }
+
+        public void Block()
+        {
+            if (_dashCoroutine != null) ClearCoroutine();
+            IsBlocked = true;
+        }
+
+        public void UnBlock()
+        {
+            if (_dashCoroutine != null) ClearCoroutine();
+            IsBlocked = false;
+        }
+
+        private void ClearCoroutine()
+        {
+            StopCoroutine(_dashCoroutine);
+            _dashCoroutine = null;
+            _rigidbody.linearVelocity = Vector2.zero;
+        }
+        
+        private void OnDestroy()
+        {
+            _dash.performed -= Dash;
+        }
+    }
+}
---
+++ Assets/Game/Scripts/Utility/IBlockable.cs
@@ -0,0 +1,10 @@
+﻿namespace Game.Scripts.Utility
+{
+    public interface IBlockable
+    {
+        public bool IsBlocked { get; }
+
+        public void Block();
+        public void UnBlock();
+    }
+}
