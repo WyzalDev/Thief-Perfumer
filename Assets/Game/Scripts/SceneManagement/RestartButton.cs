4df7e096df164533b47a04e37824088aadb8800a
WyzalDev <wyzaldev@gmail.com>
2025-06-15 07:22:05 +0300

    FirstBuild changes

---
+++ Assets/Game/Scripts/SceneManagement/RestartButton.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+namespace Game.Scripts.SceneManagement
+{
+    public class RestartButton : MonoBehaviour
+    {
+        public void Restart()
+        {
+            StartCoroutine(SceneLoader.LoadScene(SceneName.Tutorial1));
+        }
+    }
+}
